613b8ca70b046a684fee4029a253dda2026aa47b
Mathew <mpagan3@huskers.unl.edu>
2025-08-28 11:59:15 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Mathew Pagan
+ *         mpagan3@huskers.unl.edu
+ * Date: 2025/08/28
+ *
+ * Program that prints my name and major for Hack 1
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Mathew Pagan\n");
+  return 0;
+}
